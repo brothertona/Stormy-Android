1b3ccf1c1d5ffb6357c3c38c7cd7d27addb62afd
brothertona <brothermanandrew@gmail.com>
2015-06-28 22:13:04 -0400

    Finished app

---
+++ app/src/main/java/com/bignerdranch/android/stormy/ui/AlertDialogFragment.java
@@ -0,0 +1,26 @@
+package com.bignerdranch.android.stormy.ui;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+import com.bignerdranch.android.stormy.R;
+
+/**
+ * Created by andrew on 6/16/2015.
+ */
+public class AlertDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+        .setTitle(context.getString(R.string.error_title))
+                .setMessage(context.getString(R.string.error_message))
+                .setPositiveButton(context.getString(R.string.error_ok_button_text), null);
+
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+}
